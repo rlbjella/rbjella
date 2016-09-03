73211fb304bfe9391b5a052c680ceec37db4fb13
rlbjella <russell.bjella@colorado.edu>
2016-09-02 23:36:07 -0600

    Fixed strings (again)

--- parser.py
+++ parser.py
@@ -6,16 +6,24 @@ import re
 
 lines = [line.rstrip('\n') for line in open('aux/listing.txt')]
 
-print(str(len(lines))+" animations found.")
+print(str(len(lines))+" anims found.")
 
+# split anims from filenames
 files = []
 anims = []
-for l in lines:
-	file,anim = l.split(":")
-	files.extend(file)
-	anims.extend(anim)
-	print(file)
-	print(anim)
+for index in range(len(lines)):
+	[file,anim]=lines[index].split()
+	files.append(file)
+	anims.append(anim)
 
-print(str(len(files)))
-print(str(len(anims)))
+# sort anims and files and make lowercase
+for file in files:
+	file = file.lower()
+for anim in anims:
+ 	anim.lower()
+files.sort()
+anims.sort()
+
+for i in range(len(lines)):
+	if(anims[i] == anims[i-1]):
+		print('duplicate '+files[i]+': ' +anims[i] + ' in ' + files[i-1])
